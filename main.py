0d9d05e798e5056b02d3e8f42ba989961d8ae9ee
Luis Mori Guerra <luismorith@gmail.com>
2024-04-25 19:58:58 -0500

    feat: adding ui
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- main.py
+++ main.py
@@ -13,54 +13,72 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 
 
+def process_input(urls, questions):
+    model_local = ChatOllama(model='mistral')
 
-model_local = ChatOllama(model='mistral')
+    # urls = [
+    #     "https://ollama.com/",
+    #     "https://ollama.com/blog/windows-preview",
+    #     "https://ollama.com/blog/openai-compatibility",
+    # ]
+    
+    url_list = urls.split("\n")
 
-urls = [
-    "https://ollama.com/",
-    "https://ollama.com/blog/windows-preview",
-    "https://ollama.com/blog/openai-compatibility",
-]
-
-docs = [WebBaseLoader(url).load() for url in urls]
-
-docs_list = [item for sublist in docs for item in sublist]
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=7500, chunk_overlap=100, add_start_index=True
-)
-doc_splits = text_splitter.split_documents(docs_list)
-
-
-vecorstore = Chroma.from_documents(
-    documents=doc_splits,
-    collection_name="rag-chroma",
-    embedding=OllamaEmbeddings(model='nomic-embed-text'),
-)
-
-retriever = vecorstore.as_retriever()
-
-# 3. before rag
-print("before rag\n")
-before_rag_template ="what is {topic}"
-before_rag_prompt = ChatPromptTemplate.from_template(before_rag_template)
-before_rag_chain = before_rag_prompt | model_local | StrOutputParser()
-print(before_rag_chain.invoke({"topic": "Ollama"}))
+    docs = [WebBaseLoader(url).load() for url in url_list]
+    docs_list = [item for sublist in docs for item in sublist]
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=7500, chunk_overlap=100, add_start_index=True
+    )
+    doc_splits = text_splitter.split_documents(docs_list)
 
 
-# 4. after rag
-print("##########\n")
-print("after rag\n")
-after_rag_template= """Answer the question based only on the following context:
-{context}
-Question: {question}
-"""
+    vecorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=OllamaEmbeddings(model='nomic-embed-text'),
+    )
 
-after_rag_prompt= ChatPromptTemplate.from_template(after_rag_template)
-after_rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | after_rag_prompt
-    | model_local
-    | StrOutputParser()
-)
+    retriever = vecorstore.as_retriever(search_kwargs={'k': 1})
 
-print(after_rag_chain.invoke("What is Ollama?"))
+    # # 3. before rag
+    # print("before rag\n")
+    # before_rag_template ="what is {topic}"
+    # before_rag_prompt = ChatPromptTemplate.from_template(before_rag_template)
+    # before_rag_chain = before_rag_prompt | model_local | StrOutputParser()
+    # print(before_rag_chain.invoke({"topic": "Ollama"}))
+
+
+    # 4. after rag
+    print("##########\n")
+    print("after rag\n")
+    after_rag_template= """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+
+    after_rag_prompt= ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | after_rag_prompt
+        | model_local
+        | StrOutputParser()
+    )
+
+    return after_rag_chain.invoke(questions)
+
+# Define Gradio interface
+iface = gr.Interface(fn=process_input,
+                     inputs=[
+                         gr.Textbox(label="Enter URLs separated by new lines"), 
+                         gr.Textbox(label="Question")],
+                     outputs="text",
+                     title="Document Query with Ollama",
+                     description="Enter URLs and a question to query the documents.")
+iface.launch()
+
+## urls
+# https://ollama.com/
+# https://ollama.com/blog/windows-preview
+# https://ollama.com/blog/openai-compatibility
+## question
+# what is ollama ?
